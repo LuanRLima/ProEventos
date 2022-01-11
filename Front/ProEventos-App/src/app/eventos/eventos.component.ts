529b0d58b928f9085fa828a3645a85711ade2b12
LuanRLima <luanrlima9@gmail.com>
2022-01-11 04:37:35 -0300

    :sparkles:  creating search and show photo button

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,17 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  larguraImagem : number = 60;
+  margemImagem : number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+  public set filtroLista(valor: string) {
+    this._filtroLista = valor;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response),
-      error => console.log(error);
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+      error => console.log(error)
+    );
   }
 
 }
